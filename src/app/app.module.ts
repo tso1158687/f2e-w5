54f8b031f265298ade2a037ac2e1fcc3beda909b
jason zheng <101104044@nccu.edu.tw>
2018-07-08 13:32:48 +0800

    一個段落

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TabViewModule} from 'primeng/tabview';
 import { MainComponent } from './main/main.component';
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { MainComponent } from './main/main.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
